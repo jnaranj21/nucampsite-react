d24b91df1c6ab8af10c806a05aa40ad49f7df8bf
Juan <juan21.naranjo@gmail.com>
2023-03-25 17:22:05 +0100

    AboutPage, Partner and PartnersList

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsiteDetailPage from "./pages/CampisteDetailPage";
+import AboutPage from "./pages/AboutPage";
 import "./App.css";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
         <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
