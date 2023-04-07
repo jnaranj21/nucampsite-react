102fba8959e861021b84cd724ee493dddf3d6695
Juan <juan21.naranjo@gmail.com>
2023-04-07 22:08:14 +0200

    Redux campsitesSlice Campsites useSelector() Redux for all, add comment, add Middleware

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,27 +1,28 @@
-import { Col } from 'reactstrap';
-import Comment from './Comment';
-import { selectCommentsByCampsiteId } from './commentsSlice';
-import CommentForm from './CommentForm';
+import { Col } from "reactstrap";
+import Comment from "./Comment";
+import { selectCommentsByCampsiteId } from "./commentsSlice";
+import CommentForm from "./CommentForm";
+import { useSelector } from "react-redux";
 
 const CommentsList = ({ campsiteId }) => {
-    const comments = selectCommentsByCampsiteId(campsiteId);
+  const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
-    if(comments && comments.length > 0) {
-        return (
-            <Col md='5' className='m-1'>
-                <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
-                <CommentForm campsiteId={campsiteId} />
-            </Col>
-        )
-    }
+  if (comments && comments.length > 0) {
     return (
-        <Col md='5' className='m-1'>
-            There are no comments for this campsite yet. 
-        </Col>
-    )
+      <Col md="5" className="m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return <Comment key={comment.id} comment={comment} />;
+        })}
+        <CommentForm campsiteId={campsiteId} />
+      </Col>
+    );
+  }
+  return (
+    <Col md="5" className="m-1">
+      There are no comments for this campsite yet.
+    </Col>
+  );
 };
 
 export default CommentsList;
